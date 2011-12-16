7186d69d07adebf82db57d51656434e6b27bc6cb
Francois LAROCHE <laroche.f@sfeir.com>
2011-12-16 13:36:27 +0100

    Move licence elements to the top of the page

--- gwt-recaptcha/src/com/sfeir/captcha/server/CaptchaValidator.java
+++ gwt-recaptcha/src/com/sfeir/captcha/server/CaptchaValidator.java
@@ -1,14 +1,4 @@
 /**
- * 
- */
-package com.sfeir.captcha.server;
-
-import net.tanesha.recaptcha.ReCaptchaImpl;
-import net.tanesha.recaptcha.ReCaptchaResponse;
-
-import com.sfeir.captcha.shared.CaptchaResult;
-
-/**
  * Copyright (c) 2011 François LAROCHE
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
@@ -20,9 +10,15 @@ import com.sfeir.captcha.shared.CaptchaResult;
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
- * <br />
- * 
+ */
+package com.sfeir.captcha.server;
+
+import net.tanesha.recaptcha.ReCaptchaImpl;
+import net.tanesha.recaptcha.ReCaptchaResponse;
+
+import com.sfeir.captcha.shared.CaptchaResult;
+
+/**
  * Class used to validate the captcha server side.<br />
  * You are not obliged to use it, you can use the ReCaptchaImpl class provided in recaptcha4j.
  * 
--- gwt-recaptcha/src/com/sfeir/captcha/shared/CaptchaResult.java
+++ gwt-recaptcha/src/com/sfeir/captcha/shared/CaptchaResult.java
@@ -1,11 +1,4 @@
 /**
- * 
- */
-package com.sfeir.captcha.shared;
-
-import java.io.Serializable;
-
-/**
  * Copyright (c) 2011 François LAROCHE
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
@@ -17,9 +10,12 @@ import java.io.Serializable;
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * 
- * <br />
- * 
+ */
+package com.sfeir.captcha.shared;
+
+import java.io.Serializable;
+
+/**
  * Transport class to validate the captcha from the client to the server
  * 
  * @author François LAROCHE
